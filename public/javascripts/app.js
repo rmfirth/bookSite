18ec434a807754dfa9d9e433c3cc6e0895080cfe
Ryan Firth <rmfirth@gmail.com>
2017-11-17 09:59:26 -0700

    a

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,14 +3,21 @@ angular.module('book', [])
        '$scope',
        '$http',
         function($scope,$http) {
+            $scope.books = [];
+
+
             $scope.addComment = function(book) {
                 if($scope.userName === '' || $scope.commentText === '') {return;}
 
             };
 
             $scope.addBook = function() {
-
-            }
+                bookQuery = $scope.searchQuery.split(' ').join('+');
+                console.log(bookQuery);
+                return $http.post('/books', bookQuery).success(function(data){
+                   $scope.books.push(data);
+                });
+            };
 
             $scope.create = function(book) {
                 return $http.post('/bookComment',comment).success(function(data){
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var request = require('request');
+var parseString = require('xml2js').parseString;
+var BookSchema = new mongoose.Schema({
+    title:String,
+    isbn:String,
+    reviews: [{text:String, userID:String}]
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+
+router.get('/books', function(req,res,next) {
+  Book.find(function(err,books){
+    if(err){return next(err);}
+    res.json(books);
+  });
+});
+
+router.post('/books', function(req,res,next) {
+  var searchBook = req.body;
+  var goodReads = "https://www.goodreads.com/search/?q=";
+  var APIkey = "&key=kHFSG3T2ByzOJEZcpEw9vw";
+  goodReads = goodReads + searchBook + APIkey;
+  var response = request(goodReads);
+  parseString(response, function(err,result) {
+    console.log(JSON.stringify(result));
+  });
+
+});
 
 
 module.exports = router;
